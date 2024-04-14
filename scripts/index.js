306ddc95dfe9ca53bd05dc102325f5189a68f282
Denis Chornyi <chornyi.den@gmail.com>
2024-04-14 13:13:36 +0200

    calendar

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import { renderTimescale } from './calendar/timescale.js';
 import { renderWeek, renderDecoration } from './calendar/calendar.js';
 import { renderHeader } from './calendar/header.js';
 import { initNavigation } from './header/navigation.js';
-import { getEvents, getItem, setItem } from './common/storage.js';
+import { setItem } from './common/storage.js';
 import { getStartOfWeek } from './common/time.utils.js';
 import { initEventForm } from './events/createEvent.js';
 import { timeLine } from './calendar/timeline.js';
